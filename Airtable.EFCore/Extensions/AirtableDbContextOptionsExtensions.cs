52190cbc67b1f409810b6097c4c83eeaf0f9f9ad
Zach Wolfe <zachrwolfe@me.com>
2025-05-08 20:18:57 -0500

    Add UseAirtable overload that takes connection string

--- Airtable.EFCore/Extensions/AirtableDbContextOptionsExtensions.cs
+++ Airtable.EFCore/Extensions/AirtableDbContextOptionsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Airtable.EFCore.Infrastructure;
+﻿using Airtable.EFCore;
+using Airtable.EFCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Microsoft.EntityFrameworkCore;
@@ -31,6 +32,23 @@ public static class AirtableDbContextOptionsExtensions
         airtableOptionsAction?.Invoke(new AirtableDbContextOptionsBuilder(optionsBuilder));
 
         return optionsBuilder;
+    }
+
+    public static DbContextOptionsBuilder<TContext> UseAirtable<TContext>(
+        this DbContextOptionsBuilder<TContext> optionsBuilder,
+        string connectionString,
+        Action<AirtableDbContextOptionsBuilder>? airtableOptionsAction = null)
+        where TContext : DbContext
+        => (DbContextOptionsBuilder<TContext>)UseAirtable(
+            (DbContextOptionsBuilder)optionsBuilder, connectionString, airtableOptionsAction);
+
+    public static DbContextOptionsBuilder UseAirtable(
+        this DbContextOptionsBuilder optionsBuilder,
+        string connectionString,
+        Action<AirtableDbContextOptionsBuilder>? airtableOptionsAction = null)
+    {
+        var parseResult = new AirtableDatabaseConnectionStringBuilder(connectionString);
+        return optionsBuilder.UseAirtable(parseResult.BaseId, parseResult.ApiKey, airtableOptionsAction);
     }
 
     private static void ConfigureWarnings(DbContextOptionsBuilder optionsBuilder)
